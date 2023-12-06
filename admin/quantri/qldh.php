aa7feabcd0904631c56aad1f2ce5d6c7449bba59
docaohaidang04 <127119872+docaohaidang04@users.noreply.github.com>
2023-12-07 01:16:44 +0700

    Update qldh.php

--- admin/quantri/qldh.php
+++ admin/quantri/qldh.php
@@ -4,13 +4,13 @@ $stt = 0;
 foreach ($showb as $value) {
     extract($value);
     $stt++;
+
     $kq .= '<tr>
         <td>' . $stt . '</td>
         <td>' . $nguoidat_ten . '</td>
         <td>' . $nguoidat_dienthoai . '</td>
         <td>' . $nguoidat_diachi . '</td>
         <td>' . $nguoidat_email . '</td>
-        <td>đang xử lý</td>
         <td>' . $total . '</td>
         <td>' . $ngaydat . '</td>
         <td><a href="admin.php?pg=chitietlichsu&id=' . $id . '"><i class="fas fa-eye" style="color: #ff0000;"></i></a></td>
@@ -31,8 +31,8 @@ foreach ($showb as $value) {
             <th>SĐT người đặt</th>
             <th>Địa chỉ người đặt</th>
             <th>Email người đặt</th>
-            <th>Trạng thái</th>
             <th>Tổng tiền</th>
+
             <th>Ngày đặt</th>
             <th>Chi tiết đơn hàng</th>
         </tr>
@@ -42,14 +42,14 @@ foreach ($showb as $value) {
 </div>
 
 <style>
-.content {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
+    .content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 
-table {
-    margin: 0 auto;
-    width: 90%;
-}
+    table {
+        margin: 0 auto;
+        width: 90%;
+    }
 </style>
